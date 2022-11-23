231beae4d420d46129bdd3ad2c5b8b21f2d761de
Edmilson Corrêa <edm.correa@hotmail.com>
2022-11-23 17:40:35 -0300

    18 - Develop in /app/backend/src/database in the corresponding folders, a migration and a model for the matches table [Requirement 18]

---
+++ app/backend/src/database/models/Matches.ts
@@ -0,0 +1,42 @@
+import { DataTypes, Model } from 'sequelize';
+import db from '.';
+// import OtherModel from './OtherModel';
+
+class Matches extends Model {
+  declare id: number;
+  declare homeTeam: number;
+  declare homeTeamGoals: number;
+  declare awayTeam: number;
+  declare awayTeamGoals: number;
+  declare inProgress: boolean;
+}
+
+Matches.init({
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+  },
+  homeTeam: DataTypes.INTEGER,
+  homeTeamGoals: DataTypes.INTEGER,
+  awayTeam: DataTypes.INTEGER,
+  awayTeamGoals: DataTypes.INTEGER,
+  inProgress: DataTypes.BOOLEAN,
+}, {
+  underscored: true,
+  sequelize: db,
+  timestamps: false,
+  tableName: 'matches',
+});
+
+/**
+  * `Workaround` para aplicar as associations em TS:
+  * Associations 1:N devem ficar em uma das instâncias de modelo
+  * */
+
+// OtherModel.belongsTo(Example, { foreignKey: 'campoA', as: 'campoEstrangeiroA' });
+// OtherModel.belongsTo(Example, { foreignKey: 'campoB', as: 'campoEstrangeiroB' });
+
+// Example.hasMany(OtherModel, { foreignKey: 'campoC', as: 'campoEstrangeiroC' });
+// Example.hasMany(OtherModel, { foreignKey: 'campoD', as: 'campoEstrangeiroD' });
+
+export default Matches;
