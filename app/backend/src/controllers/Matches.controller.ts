e897b9ed40254ca2a437329aa6d7f8f77663fe8c
Edmilson Corrêa <edm.correa@hotmail.com>
2022-11-28 11:07:48 -0300

    20 - Develop the /matches endpoint so that it is possible to filter matches in progress on the front-end matches screen [Requirement 20]

--- app/backend/src/controllers/Matches.controller.ts
+++ app/backend/src/controllers/Matches.controller.ts
@@ -4,7 +4,20 @@ import MatchesService from '../services/Matches.service';
 export default class MatchesController {
   constructor(private matchesService = new MatchesService()) {}
   getMatches = async (req: Request, res: Response) => {
+    const { inProgress } = req.query;
+    console.log(inProgress);
+
     const getAll = await this.matchesService.getMatches();
+    if (inProgress) {
+      const filtered = getAll.filter((match) => match.inProgress === Boolean(inProgress));
+      return res.status(200).json(filtered);
+    }
     return res.status(200).json(getAll);
   };
+
+  // filterInProgress = async (req: Request, res: Response) => {
+  //   const { q } = req.query;
+
+  //   return res.status(200).json(q);
+  // };
 }
